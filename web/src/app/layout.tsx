c89222674b146dcb768c15a7e0defc4b2d38b005
titouwajd <titou.wajd@gmail.com>
2026-07-11 22:42:02 +0200

    Add Google Tag Manager GTM-WGJ5SNKM

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -35,7 +36,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <head>
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WGJ5SNKM');`}
+        </Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900 min-h-screen flex flex-col`}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGJ5SNKM"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+        </noscript>
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
